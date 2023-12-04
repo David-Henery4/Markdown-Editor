6c8c5f4382a49e70dcbc247c6ee62117c9d96b2f
DizzlyD <david4henery00@hotmail.com>
2023-12-04 22:55:19 +0000

    resolved-preference-issue

--- src/app/(components)/sidebar-components/ThemeToggle.jsx
+++ src/app/(components)/sidebar-components/ThemeToggle.jsx
@@ -2,27 +2,27 @@ import { DarkModeIcon, LightModeIcon } from "../../../../public/assets";
 import { useTheme } from "next-themes";
 
 const ThemeToggle = () => {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, resolvedTheme } = useTheme();
   //
   return (
     <div className="flex justify-start items-center gap-3">
       <DarkModeIcon
-        className={`${theme === "dark" ? "fill-white" : "fill-veryDarkGrey"}`}
+        className={`${resolvedTheme === "dark" ? "fill-white" : "fill-veryDarkGrey"}`}
       />
       <div
         className="relative w-12 h-6 p-[6px] rounded-[14.5px] bg-veryDarkGrey hover:cursor-pointer"
         onClick={() =>
-          theme === "dark" ? setTheme("light") : setTheme("dark")
+          resolvedTheme === "dark" ? setTheme("light") : setTheme("dark")
         }
       >
         <div
           className={`w-3 h-3 rounded-full bg-white transition-all absolute top-1/2 left-1/2 -translate-y-1/2 ${
-            theme === "dark" ? "-translate-x-[150%]" : "translate-x-1/2"
+            resolvedTheme === "dark" ? "-translate-x-[150%]" : "translate-x-1/2"
           }`}
         ></div>
       </div>
       <LightModeIcon
-        className={`${theme === "dark" ? "fill-veryDarkGrey" : "fill-white"}`}
+        className={`${resolvedTheme === "dark" ? "fill-veryDarkGrey" : "fill-white"}`}
       />
     </div>
   );
