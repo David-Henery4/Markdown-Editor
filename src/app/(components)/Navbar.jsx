2abb2e5f58ae51b370a3e29911f13f84620c12ad
DizzlyD <david4henery00@hotmail.com>
2023-12-28 15:21:51 +0000

    added-demo-signin

--- src/app/(components)/Navbar.jsx
+++ src/app/(components)/Navbar.jsx
@@ -1,4 +1,4 @@
-import { SaveBtn, BurgerMenu, DeleteBtn, FileNameAndLogo, DownloadBtn, Dropdown } from "./navbar-components";
+import { SaveBtn, BurgerMenu, FileNameAndLogo, Dropdown } from "./navbar-components";
 
 const Navbar = () => {
   return (
@@ -9,8 +9,6 @@ const Navbar = () => {
 
       <div className="flex justify-center items-center p-2 gap-6 smTablet:p-4">
         <Dropdown/>
-        {/* <DownloadBtn/>
-        <DeleteBtn /> */}
         <SaveBtn />
       </div>
     </nav>
--- src/app/(components)/auth/footer/DemoSignIn.jsx
+++ src/app/(components)/auth/footer/DemoSignIn.jsx
@@ -1,10 +1,28 @@
 "use client"
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const DemoSignIn = () => {
+  const router = useRouter()
+  //
+  const handleDemoSignIn = async () => {
+    const data = await signIn("credentials", {
+      redirect: false,
+      username: "demo1234",
+      password: "demo123456",
+    });
+    if (data.ok){
+      router.refresh()
+      return
+    }
+    console.log("Demo Sign-in error")
+    console.error("Demo Sign-in error")
+    return
+  }
   //
   return (
     <div className="w-full text-xs mt-4 text-orange hover:text-lightOrange">
-      <button>(Click here to login with a demo account)</button>
+      <button onClick={handleDemoSignIn}>(Click here to login with a demo account)</button>
     </div>
   );
 }
