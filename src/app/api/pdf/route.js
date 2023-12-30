d9dc7b922372701a2390e173a8bd378c8d18dd94
DizzlyD <david4henery00@hotmail.com>
2023-12-30 01:02:36 +0000

    styles-working-on-local-&-build-(pre-styles-removal-to-test-on-vercel)

--- src/app/api/pdf/route.js
+++ src/app/api/pdf/route.js
@@ -9,13 +9,13 @@ export async function POST(req) {
     console.log("PDF CALLED");
 
     // changed to headless: "new"
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     //
     const htmlContentAndStyles = `<style>@import url(https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;600;700;800&display=swap); @import url(https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Roboto+Slab:wght@300;400;500;600;700;800&display=swap); ${currentStyles} </style>${htmlContent}`;
     //
-    await page.setContent(htmlContentAndStyles);
-    // await page.setContent(htmlContentAndStyles, { waitUntil: "networkidle2" });
+    // await page.setContent(htmlContentAndStyles);
+    await page.setContent(htmlContentAndStyles, { waitUntil: "networkidle2" });
     //
     const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
     //
